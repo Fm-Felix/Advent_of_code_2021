e697bcccc8ca50129fdae7633c4aa021ea79db47
Felix Forsström <Felix.forsstrom@hotmail.com>
2021-12-01 12:13:52 +0100

    rearange file structure

--- day01/src/solution01.c
+++ day01/src/solution01.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 80
+
 
 int main(int argc, char const *argv[])
 {
     printf("Hello world\n");
+
+    char* path;
+
+
     return 0;
 }
