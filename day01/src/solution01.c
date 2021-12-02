85b18b19d22f9199d1aac853af3103755e617a80
Felix Forsström <Felix.forsstrom@hotmail.com>
2021-12-02 12:07:03 +0100

    task one done using scanf for input

--- day01/src/solution01.c
+++ day01/src/solution01.c
@@ -5,12 +5,26 @@
 #define MAX_LINE_LENGTH 80
 
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    printf("Hello world\n");
+    int input[4096];
+    int tmp;
+    int i = 0;
 
-    char* path;
+    while (scanf("%d", &tmp) != EOF) 
+    {
+        input[i] = tmp;
+        i++;
+    }
 
-
+    int c = 0;
+    for (int i = 0; i < (sizeof(input) / sizeof(input[0]) - 1); i++)
+    {
+        if (input[i] < input[i + 1])
+            c++;
+    }
+    
+    printf("%d\n", c);
+    
     return 0;
 }
