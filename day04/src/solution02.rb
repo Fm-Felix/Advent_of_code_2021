ba77d5d72d90ca6d8c0b5b1e305b79b398cd82ff
Felix Forsström <felix.forsstrom@hotmail.com>
2021-12-23 23:47:25 +0100

    day 4 part 2 done

--- day04/src/solution02.rb
+++ day04/src/solution02.rb
@@ -1,58 +1,43 @@
-$data = [] #Array of 12-bit-sequences
+input = ARGF.read
+lines = input.split("\n")
 
-ARGF.each_line { |line|
-    $data << line.chomp.chars.map {|c| c.to_i} # populate data array
-}
+lines_per_board = 6 # including whiteline
+line_offset = 2
+num_boards = lines.count / lines_per_board
 
-def most_common_bit (d, i)
-    bits = d.map{ |line| line[i] }
-    ones = bits.count(1)
-    zeroes = bits.count(0)
-
-    if ones == zeroes
-        1
-    elsif ones < zeroes
-        0
-    else
-        1
-    end
+boards = num_boards.times.map do |idx|
+    idx *= lines_per_board
+    idx += line_offset
+    
+    lines[idx, lines_per_board - 1].map(&:split).map { |row| row.map(&:to_i) }
 end
 
-def least_common_bit (d, i)
-    bits = d.map{ |line| line[i] }
-    ones = bits.count(1)
-    zeroes = bits.count(0)
+numbers = lines[0].split(",").map(&:to_i)
 
-    if ones == zeroes
-        0
-    elsif ones < zeroes
-        1
-    else
-        0
-    end
+def draw_numbers (numbers, boards)
+    drawn = []
+    seq = nil
+
+    numbers.each do |draw|
+        drawn << draw
+        board_r = nil
+
+        boards.each do |board|
+            board_r = board
+
+            board.each do |row|
+                boards.delete(board) if row.all? { |num| drawn.any?(num) }
+            end 
+
+            board.transpose.each do |col|
+                boards.delete(board) if col.all? { |num| drawn.any?(num) }
+            end
+        end
+
+        return [draw, drawn, board_r] if boards.count == 0
+    end    
 end
 
-o_g_r = $data.map(&:clone)
-c_s_v = $data.map(&:clone)
-for i in 0..$data[0].length - 1 do
-    o_g_r_inter = o_g_r.select { |line| line[i] == most_common_bit(o_g_r, i) } 
-    if o_g_r_inter.empty?
-        break
-    else
-        o_g_r = o_g_r_inter
-    end
-end
-
-for i in 0..$data[0].length - 1 do
-    c_s_v_inter = c_s_v.select { |line| line[i] == least_common_bit(c_s_v, i) } 
-    if c_s_v_inter.empty?
-        break
-    else
-        c_s_v = c_s_v_inter
-    end
-
-end
-puts o_g_r.join.to_i(2) * c_s_v.join.to_i(2)
-
-
-
+losing_draw, drawn, losing_board = draw_numbers(numbers, boards)
+sum_non_drawn = losing_board.flatten.difference(drawn).sum
+puts sum_non_drawn * losing_draw
